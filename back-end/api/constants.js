c2f1285a79c16575c858c9404291909a6e017d44
AgroC <jmikulyak@gmail.com>
2021-04-06 00:16:37 -0400

    db connection string comment

--- back-end/api/constants.js
+++ back-end/api/constants.js
@@ -1,4 +1,5 @@
 const constants = {
-    db_url: 'mongodb://localhost:27017'
+    db_url: 'mongodb://localhost:27017',
+    // db_url: 'mongodb://user:pass@localhost:27017'
 };
 module.exports.constants = constants;
