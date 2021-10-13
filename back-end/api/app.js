492ec29a4cefd6db27ad2a104eb94e5fa2b39220
AgroC <jmikulyak@gmail.com>
2021-10-12 21:45:56 -0400

    auth login reg work

--- back-end/api/app.js
+++ back-end/api/app.js
@@ -1,18 +1,35 @@
+const config = require('config');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const Joi = require('joi');
+Joi.objectId = require('joi-objectid')(Joi);
+const mongoose = require('mongoose');
+const constants = require('./constants');
+
 var user = require('./routes/routes-user');
 var requirements = require('./routes/routes-audit-reqs');
 var courses = require('./routes/routes-course');
 var documents = require('./routes/routes-document');
 var assignments = require('./routes/routes-assignment');
+var userReg = require('./routes/routes-user-reg');
+var auth = require('./routes/routes-auth');
+
 var app = express();
 
-app.use(bodyParser.json({limit:'50mb'}));
-app.use(bodyParser.urlencoded({ extended: false, limit: '50mb' }));
-app.use(cookieParser());
+if (!config.get('PrivateKey')) {
+    console.error('FATAL ERROR: PrivateKey is not defined.');
+    process.exit(1);
+}
 
+mongoose.connect(constants.constants.db_url)
+    .then(() => console.log('Now connected to MongoDB!'))
+    .catch(err => console.error('Something went wrong', err));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
@@ -25,5 +42,7 @@ app.use('/api/requirement', requirements);
 app.use('/api/course', courses);
 app.use('/api/document', documents);
 app.use('/api/assignment', assignments);
+app.use('/api/user-reg', userReg);
+app.use('/api/auth', auth);
 
 module.exports = app;
